0999f0764f93088f729be4e22e2b63dfe20eaf6e
egamovj <egamovj90@gmail.com>
2024-05-23 11:11:13 +0500

    slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -33,13 +33,13 @@ const slides = [
 const Slider = () => {
   const [current, setCurrent] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrent((prevCurrent) => (prevCurrent + 1) % slides.length);
-    }, 3000); // Change slide every 3 seconds
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrent((prevCurrent) => (prevCurrent + 1) % slides.length);
+//     }, 4000); // Change slide every 3 seconds
 
-    return () => clearInterval(interval);
-  }, []);
+//     return () => clearInterval(interval);
+//   }, []);
 
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
@@ -53,8 +53,12 @@ const Slider = () => {
             key={slide.id}
           >
             <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
-              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
-              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
               <Link href={slide.url}>
                 <button className="bg-black text-white rounded-md py-3 px-4">
                   SHOP NOW
@@ -71,6 +75,21 @@ const Slider = () => {
                 className="object-cover"
               />
             </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
           </div>
         ))}
       </div>
