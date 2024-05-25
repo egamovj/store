a062a7169096919a09ab84c06597fcde080fcc05
egamovj <egamovj90@gmail.com>
2024-05-25 15:50:55 +0500

    add item to cart in single page

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Add = () => {
-  return <div></div>;
+  const [quantity, setQuantity] = useState(1);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h4 className="font-medium">Choose a Quantity</h4>
+      <div className="">
+        <div className=""></div>
+        <div className="">
+          Only <span className="text-orange-500">4 items</span> left! {"Don't"}{" "}
+          miss it
+        </div>
+      </div>
+      <button className="w-36 text-sm rounded-3xl ring-1 ring-[#f35c7a] text-[#f35c7a] py-2 px-4 hover:bg-[#f35c7a] hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-pink-200">
+        Add to Cart
+      </button>
+    </div>
+  );
 };
 
 export default Add;
